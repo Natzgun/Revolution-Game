17b9f43a23f525c3fc1e4e5a20c92291bdf24e9e
Sumire25 <msumirer@unsa.edu.pe>
2023-07-18 23:05:22 -0500

    2d

--- View/JugadorView.h
+++ View/JugadorView.h
@@ -1,21 +1,24 @@
-
 #ifndef REVOLUTION_GAME_JUGADORVIEW_H
 #define REVOLUTION_GAME_JUGADORVIEW_H
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Animation.h"
+#include "../Util/Vector2d.h"
+#include <vector>
 
 using std::string;
 
 class JugadorView: public sf::Drawable {
-  Animation *animation;
 public:
+  Animation *animation;
+  static std::vector<sf::Texture> torso;
+  static std::vector<sf::Texture> piernas;
+
   JugadorView(int, float,std::string);
   void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
   void updateA();
   sf::Sprite &getSprite();
 };
-
 
 #endif //REVOLUTION_GAME_JUGADORVIEW_H
