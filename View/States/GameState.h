95175ad19fda48e806b1f65925f227d234c88a11
Erick <itzlilouxx@gmail.com>
2023-08-02 08:47:03 -0500

    Se ha reparado el cambio de menu a juego

---
+++ View/States/GameState.h
@@ -0,0 +1,15 @@
+//
+// Created by natzgun on 1/08/23.
+//
+
+#ifndef REVOLUTION_GAME_GAMESTATE_H
+#define REVOLUTION_GAME_GAMESTATE_H
+
+
+class GameState {
+public:
+  virtual ~GameState() {}
+};
+
+
+#endif //REVOLUTION_GAME_GAMESTATE_H
